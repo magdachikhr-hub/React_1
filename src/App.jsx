4b3026d03b73de4a4106de47785f1e1d0e785c8b
Magda Chikhradze <magda292005@gmail.com>
2026-07-02 17:59:52 +0400

    use react props

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,34 @@ function App() {
       <main className="bg-[url('/Desktop.jpg')] h-screen bg-no-repeat bg-cover">
         <Header></Header>
 
-        <div>
-          <Card></Card>
+        <div className="flex">
+          <Card
+            info={{
+              plan: "Basic",
+              sum: "19.99",
+              storage: "500 GB",
+              users: "2",
+              send: "3",
+            }}
+          ></Card>
+          <Card
+            info={{
+              plan: "Professional",
+              sum: "29.99",
+              storage: "1 TB",
+              users: "5",
+              send: "10",
+            }}
+          ></Card>
+          <Card
+            info={{
+              plan: "Master",
+              sum: "39.99",
+              storage: "2 TB",
+              users: "10",
+              send: "20",
+            }}
+          ></Card>
         </div>
       </main>
     </>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,23 @@
-function Card() {
+function Card({ info }) {
   return (
     <>
       <div className="w-87.5 rounded-[10px] bg-white p-7.75 flex flex-col items-center">
-        <span className="text-lg text-[#6E728E] font-bold">Basic</span>
-        <span className="text-7xl text-[#4A4D60] font-bold">$19.99</span>
-        <div className="w-full flex flex-col items-center">
+        <span className="text-lg text-[#6E728E] font-bold">{info.plan}</span>
+        <span className="text-7xl text-[#4A4D60] font-bold tracking-[-2.16px] leading-17.75 ">
+          ${info.sum}
+        </span>
+        <div className="mt-5 mb-5 w-full flex flex-col items-center">
           <div className="w-full h-px bg-[#DBDCE2] mt-3 mb-3"></div>
           <span className="text-[15px] text-[#6E728E] font-bold">
-            500 GB Storage
+            {info.storage} Storage
           </span>
           <div className="w-full h-px bg-[#DBDCE2] mt-3 mb-3"></div>
           <span className="text-[15px] text-[#6E728E] font-bold">
-            2 Users Allowed
+            {info.users} Users Allowed
           </span>
           <div className="w-full h-px bg-[#DBDCE2] mt-3 mb-3"></div>
           <span className="text-[15px] text-[#6E728E] font-bold">
-            Send up to 3 GB
+            Send up to {info.send} GB
           </span>
           <div className="w-full h-px bg-[#DBDCE2] mt-3 mb-3"></div>
         </div>
@@ -25,3 +27,5 @@ function Card() {
 }
 
 export default Card;
+
+//props
